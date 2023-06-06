0762b63b42f2cf9ec3b2e5595d203b1e47cee2c6
Diky Putra <joykey.grapichdiky@gmail.com>
2023-06-07 00:36:43 +0700

    first

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,9 @@
+<nav class="navbar navbar-dark bg-dark">    
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand m-3" href="#">Beranda</a>
+    </div>
+    <button class="btn-lg shadow-lg rounded-pill btn-primary navbar-btn m-3">LOG IN</button>
+  </div>
+</nav>
+<!-- END NAVBAR -->
---
+++ resources/views/pages/beranda.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.main')
+@section('container')
+<div class="container-fluid p-5">
+    <div class='row'>
+        <div class="col-sm-4">
+            <div class="card mb-3">
+                <div class="card-header">
+                    <span class="badge rounded-pill bg-primary text-dark">Author: Sule</span>
+                </div>
+                <img src="https://wartapoin.com/wp-content/uploads/2023/03/pp-kosong.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    <button type="button" class="btn btn-success">Success</button>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4">
+            <div class="card mb-3">
+                <div class="card-header">
+                    <span class="badge rounded-pill bg-primary text-dark">Author: Sule</span>
+                </div>
+                <img src="https://wartapoin.com/wp-content/uploads/2023/03/pp-kosong.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    <button type="button" class="btn btn-success">Success</button>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4">
+            <div class="card mb-3">
+                <div class="card-header">
+                    <span class="badge rounded-pill bg-primary text-dark">Author: Sule</span>
+                </div>
+                <img src="https://wartapoin.com/wp-content/uploads/2023/03/pp-kosong.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    <button type="button" class="btn btn-success">Success</button>
+                </div>
+            </div>
+        </div>  
+    </div>
+</div>
+@endsection
